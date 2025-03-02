8c9c3f1c9084e3ad76992a01f3cc924be36698c0
Jay Kumar Sharma <157815432+jaykumar108@users.noreply.github.com>
2025-03-02 11:48:38 +0530

    Updated project

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 
 // DATABASE CONNECTION
